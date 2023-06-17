ab161e5f6226361aa63df363ff6761d475172d6c
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2023-06-17 18:05:07 +0900

    refactor : 파이썬 네이밍 규칙(Naming Rule)에 맞춰서 변수명, 메서드명 리펙토링, 고정된 값 상수 적용

--- final_test/lotto.py
+++ final_test/lotto.py
@@ -1,28 +1,38 @@
+# Python's Naming Rule(네이밍 규칙)(변수, 상수, 함수, 클래스 등의 이름에 권장되는 규칙)
+# 메서드명(소문자 + 밑줄(의미 단위로 밑줄 나누기))
+# 변수명(소문자 + 밑줄(의미 단위로 밑줄 나누기))
+# 전역 변수명(g + 밑줄 + 변수명)
+# 상수명(모두 대문자로 사용)
+
 import random
 
-alpha_index = ['A', 'B', 'C', 'D', 'E']
+FIRST_NUMBER = 1
+LAST_NUMBER = 45
+ONE_LOTTO_LENGTH = 6
+ALPHA_INDEX = ['A', 'B', 'C', 'D', 'E']
 
 
-def getWinningLottoNumbersList():
-    winningNumber = set()
-    while len(winningNumber) < 6:
-        winningNumber.add(random.randint(1, 45))
-    winningNumber = list(winningNumber)
-    winningNumber.sort()
-    return winningNumber
+def get_winning_lotto_numbers_list():
+    winning_numbers_list = set()
+    while len(winning_numbers_list) < ONE_LOTTO_LENGTH:
+        winning_numbers_list.add(random.randint(FIRST_NUMBER, LAST_NUMBER))
+    return sorted(winning_numbers_list)
 
 
-def getUserLottoNumbersList():
-    number = set()
-    while len(number) < 6:
-        number.add(random.randint(1, 45))
-    number = list(number)
-    number.sort()
-    return number
+def get_user_lotto_numbers_list():
+    user_lotto_numbers_list = set()
+    while len(user_lotto_numbers_list) < ONE_LOTTO_LENGTH:
+        user_lotto_numbers_list.add(random.randint(1, 45))
+    return sorted(user_lotto_numbers_list)
 
 
-def getBonusNumber():
-    return random.randint(1, 45)
+def print_user_lotto_numbers_list(user_lotto_numbers_list):
+    for number in user_lotto_numbers_list:
+        print(number, end='  ')
+
+
+def get_bonus_number():
+    return random.randint(FIRST_NUMBER, LAST_NUMBER)
 
 
 def compare_lotto_numbers(user_lotto_numbers, winning_numbers, bonus_number):
@@ -39,18 +49,30 @@ def compare_lotto_numbers(user_lotto_numbers, winning_numbers, bonus_number):
     return [count, bonus_count]
 
 
-def getMatchNumbers(user_lotto_numbers, winning_numbers, bonus_number):
-    matchedNumbers = []
+def get_matching_numbers(user_lotto_numbers, winning_numbers, bonus_number):
+    matching_numbers_list = []
     for number in user_lotto_numbers:
         if number in winning_numbers:
-            matchedNumbers.append(number)
+            matching_numbers_list.append(number)
 
     if bonus_number in user_lotto_numbers:
-        matchedNumbers.append(number)
-    return matchedNumbers
+        matching_numbers_list.append(bonus_number)
+    return matching_numbers_list
 
 
-def getRank(count_list):
+def print_matching_numbers(match_numbers_list):
+    if not match_numbers_list:
+        print('당첨 번호가 하나도 없습니다.', end=' ')
+    else:
+        print(f'당첨 번호 : ', end=' ')
+        for number in match_numbers_list:
+            if number == bonus_number:
+                print(f', 보너스 넘버 {number}', end=' ')
+            else:
+                print(f'{number}', end=' ')
+
+
+def print_rank(count_list):
     if count_list[0] == 6:
         print(f'일치하는 개수: {count_list[0]}, 1등')
     elif count_list[0] == 5:
@@ -66,28 +88,52 @@ def getRank(count_list):
         print(f'일치하는 개수 3개 이하 꽝 !')
 
 
-# 로또 당첨 번호
-winningNumbers = getWinningLottoNumbersList()
-bonusNumber = getBonusNumber()
-print(f'로또 당첨 번호: {winningNumbers}')
-print(f'보너스 번호: {bonusNumber}')
+def ask_whether_to_play_a_game():
+    return input('로또 게임을 시작할 것인가요 ? (y/n)')
 
-# 사용자가 구매한 번호
 
-for i in range(0, 5):
-    print(f'{alpha_index[i]}  자 동 ', end=' ')
-    userNumbers = getUserLottoNumbersList()
-    for number in userNumbers:
-        print(number, end='  ')
-    getRank(compare_lotto_numbers(userNumbers, winningNumbers, bonusNumber))
-    matchedNumbersList = getMatchNumbers(userNumbers, winningNumbers, bonusNumber)
-    if not matchedNumbersList:
-        print('당첨 번호가 하나도 없습니다.')
+def ask_whether_to_continue_playing_a_game():
+    return input('로또 게임을 다시 진행하실건가요 ? (c/q)')
+
+
+def ask_how_many_lotto_tickets_to_buy():
+    return int(input('구매할 로또 장 수: '))
+
+
+while True:
+    command = ask_whether_to_play_a_game()
+    if command == 'y':
+        user_buying_lotto_ticket_count = ask_how_many_lotto_tickets_to_buy()
+        # 로또 당첨 번호
+        winning_lotto_numbers_list = get_winning_lotto_numbers_list()
+        bonus_number = get_bonus_number()
+
+        for j in range(0, user_buying_lotto_ticket_count):
+            # 사용자가 구매한 번호
+            for i in range(0, 5):
+                print(f'{ALPHA_INDEX[i]}  자 동 ', end=' ')
+                user_lotto_numbers_list = get_user_lotto_numbers_list()
+                print_user_lotto_numbers_list(user_lotto_numbers_list)
+                print_rank(compare_lotto_numbers(user_lotto_numbers_list, winning_lotto_numbers_list, bonus_number))
+                match_numbers_list = get_matching_numbers(user_lotto_numbers_list, winning_lotto_numbers_list, bonus_number)
+                print_matching_numbers(match_numbers_list)
+                print()
+
+        print(f'로또 당첨 번호: {winning_lotto_numbers_list}')
+        print(f'보너스 번호: {bonus_number}')
+
+        restart_game_option = ask_whether_to_continue_playing_a_game()
+        if restart_game_option == 'c':
+            continue
+        elif restart_game_option == 'q':
+            break
+        else:
+            print('잘못된 명령을 입력하였습니다.')
+    elif command == 'n':
+        break
     else:
-        print(f'당첨 번호 : ', end=' ')
-        for number in matchedNumbersList:
-            if number == bonusNumber:
-                print(f', 보너스 넘버 {number}', end=' ')
-            else:
-                print(f'{number}', end=' ')
-    print()
+        print('잘못된 명령을 입력하였습니다.')
+
+print('게임을 종료합니다.')
+
+
